99c1f0cc46cccf358897d5ff28b3a5e118dd2c6c
mrpoore <rpoore@neisd.net>
2017-04-05 10:40:17 -0500

    Added class with main method to start game.

---
+++ Zork/src/Go.java
@@ -0,0 +1,18 @@
+/**
+ * A simple method to create and start a game
+ * @author Richard Poore
+ *
+ */
+public class Go {
+
+	/**
+	 * main method
+	 * Creates and starts a game
+	 * @param args - command line arguments
+	 */
+	public static void main(String[] args) {
+		Game hi = new Game();
+		hi.play();
+	}
+
+}
