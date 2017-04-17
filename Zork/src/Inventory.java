5e482757495c23a534bb116b17c823c2bc733307
mrpoore <rpoore@neisd.net>
2017-04-17 13:58:59 -0500

    Added basic structure for inventory system

---
+++ Zork/src/Inventory.java
@@ -0,0 +1,16 @@
+
+public class Inventory {
+
+	public Item[] inv;
+	
+	public Inventory()
+	{
+		inv = new Item[10];
+	}
+	
+	public Inventory(int items)
+	{
+		inv = new Item[items];
+	}
+	
+}
---
+++ Zork/src/Item.java
@@ -0,0 +1,8 @@
+
+public class Item {
+
+	private String name;
+	private int att;
+	private int dev;
+	private int keyNum;
+}
