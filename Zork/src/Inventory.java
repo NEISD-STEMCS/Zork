1c0e5b5fe153dbc5bbf7e68804c928ff441c4439
mrpoore <rpoore@neisd.net>
2017-04-17 14:48:15 -0500

    Added inventory functionality

--- Zork/src/Inventory.java
+++ Zork/src/Inventory.java
@@ -1,16 +1,45 @@
 
 public class Inventory {
 
+	int size = 0;
 	public Item[] inv;
 	
+	/**
+	 * Inventory constructor
+	 * Creates the default blank inventory with
+	 * 10 spaces
+	 */
 	public Inventory()
 	{
 		inv = new Item[10];
 	}
 	
+	/**
+	 * Inventory Constructor
+	 * Creates the blank inventory with 
+	 * <items> spaces
+	 * 
+	 * @param items the number of Items the 
+	 * inventory can hold
+	 */
 	public Inventory(int items)
 	{
 		inv = new Item[items];
+	}
+	
+	/**
+	 * This method will add an Item to the array
+	 * at position [size]
+	 * 
+	 * @param item The item to be added
+	 * @return the location in the inventory
+	 * where it was added (-1 for couldn't do it)
+	 */
+	public int add(Item item)
+	{
+		inv[size] = item;
+		size++;
+		return size-1;
 	}
 	
 }
--- Zork/src/Item.java
+++ Zork/src/Item.java
@@ -3,6 +3,41 @@ public class Item {
 
 	private String name;
 	private int att;
-	private int dev;
+	private int def;
 	private int keyNum;
+	
+	/**Creates an item with <name> as
+	* the name
+	* @param name The name of the object
+	**/
+	public Item(String name)
+	{
+		this.name = name;
+		att = 0;
+		def = 0;
+		keyNum = -1;
+	}
+	
+	/**
+	 * Creates an item will all of its attributes
+	 * 
+	 * @param name = the name of the object
+	 * @param att = the attack additive points
+	 * @param def = the defense additive points
+	 * @param keynum = the key number (-1 if not a key)
+	 */
+	public Item(String name, int att, int def, int keyNum)
+	{
+		this.name = name;
+		this.att = att;
+		this.def = def;
+		this.keyNum = keyNum;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+	
 }
